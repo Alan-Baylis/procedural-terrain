73c136d799b1f26b1a95e072f18a2ea9f3b9f0be
Max <maximmkaaa@gmailcom>
2017-07-27 19:45:22 +0300

    preview

--- Assets/Scripts/World.cs
+++ Assets/Scripts/World.cs
@@ -12,6 +12,10 @@ public class World : MonoBehaviour {
 	List<Chunk> visibleChunks = new List<Chunk> ();
 	Dictionary<Vector2, Chunk> chunks = new Dictionary<Vector2, Chunk> ();
 
+	public MeshFilter meshFilter;
+
+	public float speed = 10f;
+
 	private void Start() {
 		StartCoroutine (UpdateChunks ());
 	}
@@ -22,23 +26,75 @@ public class World : MonoBehaviour {
 			material.SetFloat ("minHeight", -heightMultiplayer);
 			material.SetFloat ("maxHeight", heightMultiplayer);
 
-			for (int z = -2; z <= 2; z++) {
-				for (int x = -2; x <= 2; x++) {
-					Vector2 coord = new Vector2 (x, z);
+			yield return Generate ();
 
-					Chunk chunk;
-					if (chunks.TryGetValue(coord, out chunk)) {
-						chunk.Update ();
-					} else {
+			//for (int z = -2; z <= 2; z++) {
+			//	for (int x = -2; x <= 2; x++) {
+			//		Vector2 coord = new Vector2 (x, z);
 
-					}
-				}
-			}
+			//		Chunk chunk;
+			//		if (chunks.TryGetValue(coord, out chunk)) {
+			//			chunk.Update ();
+			//		} else {
+			//
+			//		}
+			//	}
+			//}
 		}
 	}
 
-	public void Generate() {
-		
+	private void FixedUpdate() {
+		worldSettings.offset.y += Time.deltaTime * speed;
+	}
+
+	public bool Generate() {
+		int chunkSize = worldSettings.chunkSize;
+		int meshSize = chunkSize + 1;
+
+		int halfSize = meshSize / 2;
+
+		var meshBuilder = new MeshBuilder ();
+
+		var offset = worldSettings.offset;
+		float xOffset = offset.x;
+		float zOffset = offset.y;
+
+		float scale = worldSettings.scale;
+		float persistence = worldSettings.persistence;
+		float lacunarity = worldSettings.lacunarity;
+
+		for (int z = 0, i = 0; z < meshSize; z++) {
+			for (int x = 0; x < meshSize; x++, i++) {
+				int octaves = worldSettings.octaves;
+
+				float amplitude = 1f;
+				float frequency = 1f;
+
+				float y = 0f;
+
+				while (octaves-- > 0) {
+					float sx = (x + xOffset) / scale * frequency;
+					float sz = (z + zOffset) / scale * frequency;
+
+					float noise = Mathf.PerlinNoise (sx, sz) * 2f - 1f;
+					y += noise * amplitude;
+
+					amplitude *= persistence;
+					frequency *= lacunarity;
+				}
+
+				meshBuilder.AddVertex (x - halfSize, y * worldSettings.heightMultiplayer, z - halfSize);
+				meshBuilder.AddUV (x / (float) meshSize, z / (float) meshSize);
+				if (z < chunkSize && x < chunkSize) {
+					meshBuilder.AddTriangle (i + meshSize, i + 1, i);
+					meshBuilder.AddTriangle (i + meshSize + 1, i + 1, i + meshSize);
+				}
+			}
+		}
+
+		meshFilter.mesh = meshBuilder.Create ();
+
+		return false;
 	}
 }
 
